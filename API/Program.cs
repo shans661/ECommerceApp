77f1b21b7ec48a03b609fa72d1f18c1eecdb9f07
shans661 <shans661@gmail.com>
2025-01-08 17:49:36 +0530

    Products entity created

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,16 @@
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StoreContext>(x => {
+    x.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
