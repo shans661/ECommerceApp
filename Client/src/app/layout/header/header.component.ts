9f425b2fa305353471867dab431edafe7a0682c5
shans661 <shans661@gmail.com>
2026-09-23 21:50:29 +0530

    Header component created with simple structure

---
+++ Client/src/app/layout/header/header.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-header',
+  imports: [MatBadge, MatIcon, MatButton],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+})
+export class HeaderComponent {}
